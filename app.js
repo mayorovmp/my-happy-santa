eaca3e359d846def54ba088acf94b87bfc7b6fc8
Mayorov Maxim <mayorovmp@yandex.ru>
2018-12-19 16:38:08 +0500

    charset

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module("my-happy-santa", []);
 
-app.controller("nameController", function($scope) {
+app.controller("nameController", function($scope) 
+               {
     $scope.names = ["Марусю", "Макса", "Семена", "Галю", "Аню"];
     $scope.orders = {"МАРУСЯ":2,"МАКС":3, "СЕМЕН":1,  "ГАЛЯ":4, "АНЯ":0};
 });
